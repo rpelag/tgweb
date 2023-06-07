299bacb88de76d637bcdfb500ba46687288ff875
rpelag <135800921+rpelag@users.noreply.github.com>
2023-06-07 13:56:30 +0800

    initial

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'teamglobal_admin');
+define('DB_PASSWORD', 'P@ssw0rdP@ssw0rd');
+define('DB_NAME', 'teamglobal_user');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
