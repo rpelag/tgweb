4cf64a1f2c6bf2793b532811b3babfa1d418242b
rpelag <135800921+rpelag@users.noreply.github.com>
2023-06-13 17:10:04 +0800

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -21,69 +21,7 @@ Click here</a> to reset password.</p>';
   $row = mysqli_fetch_assoc($query);
   $expDate = $row['expDate'];
   if ($expDate >= $curDate){
-    session_start();
-
-    // Include config file
-    require_once "config.php";
-
-    // Define variables and initialize with empty values
-    $new_password = $confirm_password = "";
-    $new_password_err = $confirm_password_err = "";
-
-    // Processing form data when form is submitted
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-        // Validate new password
-        if(empty(trim($_POST["new_password"]))){
-            $new_password_err = "Please enter the new password.";
-        } elseif(strlen(trim($_POST["new_password"])) < 6){
-            $new_password_err = "Password must have atleast 6 characters.";
-        } else{
-            $new_password = trim($_POST["new_password"]);
-        }
-
-        // Validate confirm password
-        if(empty(trim($_POST["confirm_password"]))){
-            $confirm_password_err = "Please confirm the password.";
-        } else{
-            $confirm_password = trim($_POST["confirm_password"]);
-            if(empty($new_password_err) && ($new_password != $confirm_password)){
-                $confirm_password_err = "Password did not match.";
-            }
-        }
-
-        // Check input errors before updating the database
-        if(empty($new_password_err) && empty($confirm_password_err)){
-            // Prepare an update statement
-            $sql = "UPDATE users SET password = ? WHERE email = ?";
-
-            if($stmt = mysqli_prepare($link, $sql)){
-                // Bind variables to the prepared statement as parameters
-                mysqli_stmt_bind_param($stmt, "si", $param_password, $param_email);
-
-                // Set parameters
-                $param_password = password_hash($new_password, PASSWORD_DEFAULT);
-                $param_email = $email
-
-                // Attempt to execute the prepared statement
-                if(mysqli_stmt_execute($stmt)){
-                    // Password updated successfully. Destroy the session, and redirect to login page
-                    session_destroy();
-                    header("location: login.php");
-                    exit();
-                } else{
-                    echo "Oops! Something went wrong. Please try again later.";
-                }
-
-                // Close statement
-                mysqli_stmt_close($stmt);
-            }
-        }
-
-        // Close connection
-        mysqli_close($link);
-    }
-  ?>
+    ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -129,4 +67,67 @@ if($error!=""){
   echo "<div class='error'>".$error."</div><br />";
   }
 } // isset email key validate end
+
+session_start();
+
+// Include config file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Validate new password
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Please enter the new password.";
+    } elseif(strlen(trim($_POST["new_password"])) < 6){
+        $new_password_err = "Password must have atleast 6 characters.";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm the password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+
+    // Check input errors before updating the database
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        // Prepare an update statement
+        $sql = "UPDATE users SET password = ? WHERE email = ?";
+
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "si", $param_password, $param_email);
+
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_email = $email
+
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                header("location: login.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // Close connection
+    mysqli_close($link);
+}
 ?>
