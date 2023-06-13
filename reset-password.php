b287c0ac674757f7cfdfcb48a8f1512efc439466
rpelag <135800921+rpelag@users.noreply.github.com>
2023-06-13 16:52:59 +0800

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -1,5 +1,7 @@
 <?php
 include('db.php');
+session_start();
+require_once "config.php";
 
 if (isset($_GET["key"]) && isset($_GET["email"]) && isset($_GET["action"])
 && ($_GET["action"]=="reset") && !isset($_POST["action"])){
@@ -21,24 +23,52 @@ Click here</a> to reset password.</p>';
   $row = mysqli_fetch_assoc($query);
   $expDate = $row['expDate'];
   if ($expDate >= $curDate){
-  }else{
-  $error .= "<h2>Link Expired</h2>
-  <p>The link is expired. You are trying to use the expired link which
-  as valid only 24 hours (1 days after request).<br /><br /></p>";
-              }
-        }
-  if($error!=""){
-    echo "<div class='error'>".$error."</div><br />";
-    }
-  } // isset email key validate end
   ?>
-
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <title>Reset Password</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <style>
+          body{ font: 14px sans-serif; }
+          .wrapper{ width: 360px; padding: 20px; }
+      </style>
+  </head>
+  <body>
+      <div class="wrapper">
+          <h2>Reset Password</h2>
+          <p>Please fill out this form to reset your password.</p>
+          <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+              <div class="form-group">
+                  <label>New Password</label>
+                  <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
+                  <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
+              </div>
+              <div class="form-group">
+                  <label>Confirm Password</label>
+                  <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
+                  <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+              </div>
+              <div class="form-group">
+                  <input type="submit" class="btn btn-primary" value="Submit">
+                  <a class="btn btn-link ml-2" href="welcome.php">Cancel</a>
+              </div>
+          </form>
+      </div>
+  </body>
+  </html>
 <?php
-// Initialize the session
-session_start();
-
-include('db.php');
-require_once "config.php";
+}else{
+$error .= "<h2>Link Expired</h2>
+<p>The link is expired. You are trying to use the expired link which
+as valid only 24 hours (1 days after request).<br /><br /></p>";
+            }
+      }
+if($error!=""){
+  echo "<div class='error'>".$error."</div><br />";
+  }
+} // isset email key validate end
 
 // Define variables and initialize with empty values
 $new_password = $confirm_password = "";
@@ -78,7 +108,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Set parameters
             $param_password = password_hash($new_password, PASSWORD_DEFAULT);
             $param_id = $_SESSION["id"];
-            mysqli_query($con,"DELETE FROM `password_reset_temp` WHERE `email`='".$email."';");
+
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Password updated successfully. Destroy the session, and redirect to login page
@@ -98,38 +128,3 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     mysqli_close($link);
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Reset Password</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Reset Password</h2>
-        <p>Please fill out this form to reset your password.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>New Password</label>
-                <input type="password" name="new_password" class="form-control <?php echo (!empty($new_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $new_password; ?>">
-                <span class="invalid-feedback"><?php echo $new_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-link ml-2" href="welcome.php">Cancel</a>
-            </div>
-        </form>
-    </div>
-</body>
-</html>
