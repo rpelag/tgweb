e655c6665bb3099f82bf62d7ea0b75caa4a1483b
rpelag <135800921+rpelag@users.noreply.github.com>
2023-06-14 14:05:58 +0800

    removed json1.php

--- json.php
+++ json.php
@@ -1,19 +1,36 @@
 <?php
 
 include_once('config.php');
-$query="select firstname,lastname,contactnum,email,field from applicants";
-$result=mysqli_query($link,$query) or die("Error in Selecting " . mysqli_error($connection));
+$sql = "select firstname,lastname,contactnum,email,field from applicants";
+$result = mysqli_query($link, $sql);
 
-//create an array
-$emparray = array();
-while($row =mysqli_fetch_assoc($result))
-{
-    $emparray[] = $row;
+
+$data = array();
+while ($row = mysqli_fetch_assoc($result)){
+    $row_array['firstname'] = $row['firstname'];
+    $row_array['lastname'] = $row['lastname'];
+    $row_array['contactnum'] = $row['contactnum'];
+    $row_array['email'] = $row['email'];
+    $row_array['field'] = $row['field'];
+    array_push($data,$row_array);
 }
 
-echo json_encode($emparray);
+function myFilter($a) {
+    $row['firstname'] = $a['firstname'];
+    $row['lastname'] = $a['lastname'];
+    $row['contactnum'] = $a['contactnum'];
+    $row['email'] = $a['email'];
+    $row['field'] = $a['field'];
+   return $row;
+};
 
-//close the db connection
-mysqli_close($link);
+$filterData = array(
+    'total' => count($data),
+    'totalNotFiltered' => count($data),
+    'rows' => array_map("myFilter", $data)
+);
+
+$convert = json_encode($filterData);
+echo $convert;
 
 ?>
