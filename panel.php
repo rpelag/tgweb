e910c068139c609a98b1c5041047db168fda0f93
rpelag <135800921+rpelag@users.noreply.github.com>
2023-06-14 11:30:13 +0800

    Update panel.php

--- panel.php
+++ panel.php
@@ -12,40 +12,57 @@ if (isset($_SESSION["UserID"]) && $_SESSION["UserID"] === 1){
 include_once('config.php');
 $query="select * from applicants";
 $result=mysqli_query($link,$query);
-
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
 		<title> Fetch Data From Database </title>
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 	</head>
 	<body>
 	<table align="center" border="1px" style="width:900px; line-height:40px;">
-	<tr>
-		<th colspan="4"><h2>Applicants</h2></th>
-		</tr>
-			  <th> First Name </th>
-			  <th> Last Name </th>
-			  <th> Contact Number </th>
-			  <th> Email </th>
-        <th> Field of Interest </th>
-
-		</tr>
-
-		<?php while($rows=mysqli_fetch_assoc($result))
-		{
-		?>
-		<tr> <td><?php echo $rows['firstname']; ?></td>
-		<td><?php echo $rows['lastname']; ?></td>
-		<td><?php echo $rows['contactnum']; ?></td>
-		<td><?php echo $rows['email']; ?></td>
-    <td><?php echo $rows['field']; ?></td>
-		</tr>
-	<?php
-               }
+    <div class="container">
+      <h2>Bordered Table</h2>
+      <p>The .table-bordered class adds borders to a table:</p>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php while($rows=mysqli_fetch_assoc($result))
+          {
           ?>
-
-	</table>
+          <tr>
+            <td><?php echo $rows['firstname']; ?></td>
+          </tr>
+          <tr>
+            <td><?php echo $rows['lastname']; ?></td>
+          </tr>
+          <tr>
+            <td><?php echo $rows['contactnum']; ?></td>
+          </tr>
+          <tr>
+            <td><?php echo $rows['email']; ?></td>
+          </tr>
+          <tr>
+            <td><?php echo $rows['field']; ?></td>
+          </tr>
+        </tbody>
+        <?php
+                     }
+                ?>
+      </table>
+    </div>
 	</body>
 	</html>
