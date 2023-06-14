7f29edf75fbc9899ad0bea762b8e6bbd7fc02504
rpelag <135800921+rpelag@users.noreply.github.com>
2023-06-14 13:41:22 +0800

    Update panel.php

--- panel.php
+++ panel.php
@@ -18,7 +18,7 @@ $result=mysqli_query($link,$query);
 <html>
 
 <!-- Required js -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.js"></script>
