27b9a088df5f2b69bfebf48f090a60a1118ef01a
rnzslvstr <renzsilvestre04@gmail.com>
2023-06-15 13:54:05 +0800

    test forgotpage

--- forgot-passwordtest.php
+++ forgot-passwordtest.php
@@ -80,11 +80,8 @@ echo "<div class='error'>
 <p>An email has been sent to you with instructions on how to reset your password.</p>
 </div><br /><br /><br />";
 	}
-   }
-}else{
 ?>
 
-<?php } ?>
 
 <!DOCTYPE html>
 <html lang="en">
