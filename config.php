e6f87003681105ef9bcb78c465a99d405ee12451
rpelag <135800921+rpelag@users.noreply.github.com>
2023-06-15 14:28:48 +0800

    Update config.php

--- config.php
+++ config.php
@@ -13,4 +13,7 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
+date_default_timezone_set('Asia/Manila');
+$error="";
 ?>
