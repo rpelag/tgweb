0b97b247e67516ae125275b40616ae64c723923e
rnzslvstr <renzsilvestre04@gmail.com>
2023-06-15 16:56:39 +0800

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -1,22 +1,22 @@
 <?php
 include('config.php');
 
-if (isset($_GET["key"]) && isset($_GET["email"]) && isset($_GET["action"]) && ($_GET["action"]=="reset") && !isset($_POST["action"])){
+if (isset($_GET["key"]) && isset($_GET["email"]) && isset($_GET["action"]) && ($_GET["action"] == "reset") && !isset($_POST["action"])) {
   $key = $_GET["key"];
   $email = $_GET["email"];
   $curDate = date("Y-m-d H:i:s");
-  $query = mysqli_query($link, "SELECT * FROM `password_reset_temp` WHERE `key`='".$key."' and `email`='".$email."';");
+  $query = mysqli_query($link, "SELECT * FROM `password_reset_temp` WHERE `key`='" . $key . "' and `email`='" . $email . "';");
   $row = mysqli_num_rows($query);
-  
-  if ($row==""){
+  if ($row == "") {
     $error .= '<h2>Invalid Link</h2>
-    <p>The link is invalid/expired. Either you did not copy the correct link from the email, or you have already used the key in which case it is deactivated.</p>
+    <p>The link is invalid/expired. Either you did not copy the correct link
+    from the email, or you have already used the key in which case it is
+    deactivated.</p>
     <p><a href="http://172.29.105.55/~teamglobal/forgot-password.php">Click here</a> to reset password.</p>';
   } else {
     $row = mysqli_fetch_assoc($query);
     $expDate = $row['expDate'];
-    
-    if ($expDate >= $curDate){
+    if ($expDate >= $curDate) {
       ?>
       <br />
       <form method="post" action="" name="update">
@@ -28,43 +28,39 @@ if (isset($_GET["key"]) && isset($_GET["email"]) && isset($_GET["action"]) && ($
         <label><strong>Re-Enter New Password:</strong></label><br />
         <input type="password" name="pass2" maxlength="15" required/>
         <br /><br />
-        <input type="hidden" name="email" value="<?php echo $email;?>"/>
+        <input type="hidden" name="email" value="<?php echo $email; ?>"/>
         <input type="submit" value="Reset Password" />
       </form>
-      <?php
+    <?php
     } else {
       $error .= "<h2>Link Expired</h2>
-      <p>The link is expired. You are trying to use the expired link which is valid only 24 hours (1 day after request).<br /><br /></p>";
+      <p>The link is expired. You are trying to use the expired link which
+      as valid only 24 hours (1 days after request).<br /><br /></p>";
     }
   }
-  
-  if($error!=""){
-    echo "<div class='error'>".$error."</div><br />";
+  if ($error != "") {
+    echo "<div class='error'>" . $error . "</div><br />";
   }
 } // isset email key validate end
 
-if(isset($_POST["email"]) && isset($_POST["action"]) && ($_POST["action"]=="update")){
-  $error="";
-  $pass1 = mysqli_real_escape_string($link,$_POST["pass1"]);
-  $pass2 = mysqli_real_escape_string($link,$_POST["pass2"]);
+if (isset($_POST["email"]) && isset($_POST["action"]) && ($_POST["action"] == "update")) {
+  $error = "";
+  $pass1 = mysqli_real_escape_string($link, $_POST["pass1"]);
+  $pass2 = mysqli_real_escape_string($link, $_POST["pass2"]);
   $email = $_POST["email"];
   $curDate = date("Y-m-d H:i:s");
-  
-  if ($pass1!=$pass2){
-    $error.= "<p>Passwords do not match, both passwords should be the same.<br /><br /></p>";
+  if ($pass1 != $pass2) {
+    $error .= "<p>Password do not match, both password should be same.<br /><br /></p>";
   }
-  
-  if($error!=""){
-    echo "<div class='error'>".$error."</div><br />";
+  if ($error != "") {
+    echo "<div class='error'>" . $error . "</div><br />";
   } else {
     $pass1 = password_hash($pass1, PASSWORD_DEFAULT);
-    mysqli_query($link, "UPDATE `applicants` SET `password`='".$pass1."' WHERE `email`='".$email."';");
-    mysqli_query($link, "DELETE FROM `password_reset_temp` WHERE `email`='".$email."';");
-    
-    echo '<div class="success"><p>Congratulations! Your password has been updated successfully.</p>
-    <p><a href="http://172.29.105.55/~teamglobal/login.php">Click here</a> to Login.</p></div><br />';
-    header("Location: login.php");
-    exit();
+    mysqli_query($link, "UPDATE `applicants` SET `password`='" . $pass1 . "' WHERE `email`='" . $email . "';");
+    mysqli_query($link, "DELETE FROM `password_reset_temp` WHERE `email`='" . $email . "';");
+
+    echo '<script>alert("Your password has been updated successfully.");</script>';
+    echo '<script>window.location.href = "http://172.29.105.55/~teamglobal/login.php";</script>';
   }
 }
 ?>
