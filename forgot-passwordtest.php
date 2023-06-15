262c24e7d29fd6639da6b72c923410a70d2eded4
rnzslvstr <renzsilvestre04@gmail.com>
2023-06-15 15:00:38 +0800

    Update forgot-passwordtest.php

--- forgot-passwordtest.php
+++ forgot-passwordtest.php
@@ -1,100 +1,109 @@
 <?php
 include('config.php');
-if(isset($_POST["email"]) && (!empty($_POST["email"]))){
+
+if (isset($_POST["email"]) && !empty($_POST["email"])) {
     $email = $_POST["email"];
     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
     $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+
     if (!$email) {
-       $error .="<p>Invalid email address please type a valid email address!</p>";
-    }else{
-       $sel_query = "SELECT * FROM `applicants` WHERE email='".$email."'";
-       $results = mysqli_query($link,$sel_query);
-       $row = mysqli_num_rows($results);
-       if ($row==""){
-           $error .= "<p>No user is registered with this email address!</p>";
-       }
+        $error .= "<p>Invalid email address please type a valid email address!</p>";
+    } else {
+        $sel_query = "SELECT * FROM `applicants` WHERE email='".$email."'";
+        $results = mysqli_query($link, $sel_query);
+        $row = mysqli_num_rows($results);
+        
+        if ($row == "") {
+            $error .= "<p>No user is registered with this email address!</p>";
+        } else {
+            $expFormat = mktime(
+                date("H"), date("i"), date("s"), date("m"), date("d") + 1, date("Y")
+            );
+            $expDate = date("Y-m-d H:i:s", $expFormat);
+            $key = md5($email);
+            $addKey = substr(md5(uniqid(rand(), 1)), 3, 10);
+            $key = $key . $addKey;
+
+            // Insert Temp Table
+            mysqli_query($link,
+                "INSERT INTO `password_reset_temp` (`email`, `key`, `expDate`)
+                VALUES ('".$email."', '".$key."', '".$expDate."');"
+            );
+
+            $output = '<p>Dear user,</p>';
+            $output .= '<p>Please click on the following link to reset your password.</p>';
+            $output .= '<p>-------------------------------------------------------------</p>';
+            $output .= '<p><a href="http://172.29.105.55/~teamglobal/reset-password.php?key='.$key.'&email='.$email.'&action=reset" target="_blank">
+            http://172.29.105.55/~teamglobal/reset-password.php?key='.$key.'&email='.$email.'&action=reset</a></p>';
+            $output .= '<p>-------------------------------------------------------------</p>';
+            $output .= '<p>Please be sure to copy the entire link into your browser.
+            The link will expire after 1 day for security reasons.</p>';
+            $output .= '<p>If you did not request this forgotten password email, no action
+            is needed, your password will not be reset. However, you may want to log into
+            your account and change your security password as someone may have guessed it.</p>';
+            $output .= '<p>Thanks,</p>';
+            $output .= '<p>AllPHPTricks Team</p>';
+            $body = $output;
+            $subject = "Password Recovery - AllPHPTricks.com";
+
+            $email_to = $email;
+            $fromserver = "test@teamglobal.site";
+            require("PHPMailer/PHPMailerAutoload.php");
+            $mail = new PHPMailer();
+            $mail->IsSMTP();
+            $mail->Host = "112-210-226-158.cprapid.com"; // Enter your host here
+            $mail->SMTPAuth = true;
+            $mail->Username = "test@teamglobal.site"; // Enter your email here
+            $mail->Password = "P@ssw0rdP@ssw0rd"; //Enter your password here
+            $mail->SMTPSecure = "ssl";
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
+            $mail->Port = 465;
+            //$mail->SMTPDebug = 3;
+            $mail->IsHTML(true);
+            $mail->From = "test@teamglobal.site";
+            $mail->FromName = "Test";
+            $mail->Sender = $fromserver; // indicates ReturnPath header
+            $mail->Subject = $subject;
+            $mail->Body = $body;
+            $mail->AddAddress($email_to);
+            
+            if (!$mail->Send()) {
+                echo "Mailer Error: " . $mail->ErrorInfo;
+            } else {
+                echo "<script>showPrompt('success');</script>";
+            }
+        }
     }
-    if($error!=""){
-       echo "<div class='error'>".$error."</div>
-       <br /><a href='javascript:history.go(-1)'>Go Back</a>";
-    }else{
-       $expFormat = mktime(
-       date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
-       );
-       $expDate = date("Y-m-d H:i:s",$expFormat);
-       //$key = md5(2418*2+$email);
-       $key = md5($email);
-       $addKey = substr(md5(uniqid(rand(),1)),3,10);
-       $key = $key . $addKey;
-       
-       // Insert Temp Table
-       mysqli_query($link,
-       "INSERT INTO `password_reset_temp` (`email`, `key`, `expDate`)
-       VALUES ('".$email."', '".$key."', '".$expDate."');");
-       
-       $output='<p>Dear user,</p>';
-       $output.='<p>Please click on the following link to reset your password.</p>';
-       $output.='<p>-------------------------------------------------------------</p>';
-       $output.='<p><a href="http://172.29.105.55/~teamglobal/reset-password.php?key='.$key.'&email='.$email.'&action=reset" target="_blank">
-       http://172.29.105.55/~teamglobal/reset-password.php?key='.$key.'&email='.$email.'&action=reset</a></p>';
-       $output.='<p>-------------------------------------------------------------</p>';
-       $output.='<p>Please be sure to copy the entire link into your browser.
-       The link will expire after 1 day for security reason.</p>';
-       $output.='<p>If you did not request this forgotten password email, no action
-       is needed, your password will not be reset. However, you may want to log into
-       your account and change your security password as someone may have guessed it.</p>';
-       $output.='<p>Thanks,</p>';
-       $output.='<p>AllPHPTricks Team</p>';
-       $body = $output;
-       $subject = "Password Recovery - AllPHPTricks.com";
-       
-       $email_to = $email;
-       $fromserver = "test@teamglobal.site";
-       require("PHPMailer/PHPMailerAutoload.php");
-       $mail = new PHPMailer();
-       $mail->IsSMTP();
-       $mail->Host = "112-210-226-158.cprapid.com"; // Enter your host here
-       $mail->SMTPAuth = true;
-       $mail->Username = "test@teamglobal.site"; // Enter your email here
-       $mail->Password = "P@ssw0rdP@ssw0rd"; //Enter your password here
-       $mail->SMTPSecure = "ssl";
-       $mail->SMTPOptions = array(
-           'ssl' => array(
-               'verify_peer' => false,
-               'verify_peer_name' => false,
-               'allow_self_signed' => true
-           )
-       );
-       $mail->Port = 465;
-       //$mail->SMTPDebug = 3;
-       $mail->IsHTML(true);
-       $mail->From = "test@teamglobal.site";
-       $mail->FromName = "Test";
-       $mail->Sender = $fromserver; // indicates ReturnPath header
-       $mail->Subject = $subject;
-       $mail->Body = $body;
-       $mail->AddAddress($email_to);
-       if(!$mail->Send()){
-           echo "Mailer Error: " . $mail->ErrorInfo;
-       }else{
-           echo "<script>showPrompt();</script>";
-           echo "<script>setTimeout(function() { window.location.href = 'login.php'; }, 3000);</script>";
-       }
+
+    if ($error != "") {
+        echo "<div class='error'>".$error."</div>
+        <br /><a href='javascript:history.go(-1)'>Go Back</a>";
     }
-}else{
+} else {
 ?>
 <form method="post" action="" name="reset"><br /><br />
     <label><strong>Enter Your Email Address:</strong></label><br /><br />
     <input type="email" name="email" placeholder="username@email.com" />
     <br /><br />
-    <input type="submit" value="Reset Password" onclick="showPrompt()"/>
+    <input type="submit" value="Reset Password" onclick="showPrompt('reset')"/>
 </form>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <script>
-    function showPrompt() {
-        alert("An email has been sent to you with instructions on how to reset your password.");
+    function showPrompt(type) {
+        if (type === 'reset') {
+            alert("An email has been sent to you with instructions on how to reset your password.");
+        } else if (type === 'success') {
+            alert("Password reset successful.");
+            window.location.href = 'login.php';
+        }
     }
 </script>
 <?php } ?>
