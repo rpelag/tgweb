e7641f0933c25e3a2ad924312fc51ac645544a97
rpelag <135800921+rpelag@users.noreply.github.com>
2023-06-23 14:28:17 +0800

    new changes

---
+++ electricalservice.php
@@ -0,0 +1,256 @@
+<?php include 'protect.php'?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Team Global Facility Solutions Inc.</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/subcareer.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Presento - v3.10.0
+  * Template URL: https://bootstrapmade.com/presento-bootstrap-corporate-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+      <!-- <h1 class="logo me-auto"><a href="index.php">Presento<span>.</span></a></h1>-->
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <a href="jobseekeruser.php" class="logo me-auto"><img src="assets/img/TGFSi_LOGO.jpg" alt=""  ></a>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="jobseekeruser.php">Home</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+
+      </nav><!-- .navbar -->
+
+      <a href="logout.php" class="get-started-btn scrollto">Sign out</a>
+    </div>
+  </header>
+  <!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="jobseekeruser.php">Home</a></li>
+          <li><a href="engineeringsupport.php">Engineering Support</a></li>
+          <li>Electrical Services</li>
+        </ol>
+        <h2>Electrical Services</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Blog Single Section ======= -->
+    <section id="blog" class="blog">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+
+          <div class="col-lg-10 offset-md-1 entries">
+
+            <article class="entry entry-single">
+
+              <div class="entry-img">
+                <img src="assets/img/tg3.jpg" alt="" class="img-fluid">
+              </div>
+
+              <h2 class="entry-title">
+                <a>Electrical Services</a>
+              </h2>
+
+              <div class="entry-meta">
+                <ul>
+                  <li class="d-flex align-items-center"><i class="bi bi-person"></i>Electrical Services</li>
+                  <!-- <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.html"><time datetime="2020-01-01">Jan 1, 2020</time></a></li> -->
+
+                </ul>
+              </div>
+
+              <div class="entry-content">
+                <p>
+                  Specialised knowledge and skills and is primarily responsible for the installation, maintenance and repair of
+                   systems, machines and equipment while following the relevant safety measures.
+                </p>
+
+              </div>
+
+                <div class="entry-content">
+                <section id="tabs" class="tabs">
+                  <div class="container" data-aos="fade-up">
+                      <div class="tab-pane" id="tab-2">
+                        <div class="row">
+                          <p class="fst-italic">
+                            <b><h3>DUTIES AND RESPONSIBILITIES</h3></b>
+                          </p>
+                          <div class="col-lg-12 ">
+                            <ul>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Provide prompt maintenance Services including supervision for all electrical facilities of the Company.</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Handle and monitor the operation of all electrical facilities and air-conditioning units</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Repair and maintenance of all lighting facilities, convenience outlets, switches and similar devices including but not limited to generator sets, power fuse, circuit breakers, UPS and other electrical appliances and equipment.</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Implement preventive / corrective / predictive maintenance program on all electrical equipment and air-conditioning units.</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Monitor/ assist third party contractors in the repair and implementation of preventive maintenance works for air-conditioning units, fire protection system, CCTV and access control, UPS and PACU. Coordinate with Company’s Facilities Management staff any findings / recommendation of third party contractors.</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Provide technical assistance in the quarterly inspection and preventive maintenance of laboratory generator sets.</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Submit a monthly accomplishment report on the electrical maintenance services provided, including a summary of overtime rendered and recommendations to attain safe operations of the Company’s (Head Office and Laboratory) electrical facilities</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Contractor shall provide the tools necessary in the performance of the maintenance program.</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Schedule daily activities of duty electrical technicians and monitor accomplishments.</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Monitoring of electrical technicians daily time records and overtime. Contractors Electrical Engineer should submit summary of number of tardiness, absenteeism and overtime hours consumed monthly.</li></h3>
+                              <h3><li><i class="ri-checkbox-blank-circle-fill"></i>Monitoring and testing of major Generator sets for laboratory and submit proposed work program for succeeding week for approval of Company’s Agreement Officer.</li></h3>
+                            </ul>
+                          </div>
+
+
+                        </div>
+                      </div>
+                  </div>
+                </section>
+                <div class="d-flex justify-content-between read-more">
+                  <a href="engineeringsupport.php">BACK</a>
+                  <a href="mailto:engineeringHR@teamglobal.com.ph	?subject=Application for Shift Engineer">APPLY</a>
+
+                </div>
+              </div>
+
+
+              </div>
+
+            </article><!-- End blog entry -->
+
+
+
+
+
+
+        </div>
+
+      </div>
+    </section><!-- End Blog Single Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h3>TeamGlobal<span>.</span></h3>
+            <p>
+              Suite 406, 4th floor VGP Center <br>
+              6772 Ayala Ave. <br>
+              Makati City <br><br>
+              <strong>Phone:</strong> 02.817.0706/+63 917.8043211<br>
+              <strong>Email:</strong> info@teamglobal.com.ph<br>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i><a class="nav-link scrollto" href="jobseekeruser.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i><a class="nav-link scrollto" href="jobseekeruser.php#careers">Career</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Careers</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="officesupport.php">Office Support </a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="engineeringsupport.php">Engineering Support</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="itsupport.php">I.T. Support</a></li>
+            </ul>
+          </div>
+
+          <div class="social-links  col-lg-2 col-md-6 footer-links ">
+            <h4>Social Media</h4>
+            <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a> -->
+            <a href="https://www.facebook.com/teamglobalfacilitysolutions" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
+            <!-- <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+            <a href="https://www.linkedin.com/company/team-global-facility-solutions-inc-/" class="linkedin" target="_blank"><i class="bx bxl-linkedin"></i></a>
+          </div>
+
+          <!-- <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Join Our Newsletter</h4>
+            <p>Description</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div> -->
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; Copyright <strong><span>Presento</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/presento-bootstrap-corporate-template/ -->
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </div>
+
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
